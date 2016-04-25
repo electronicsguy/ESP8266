17bba82e77be6f0d551594ba15ef67fd2eb56501
Sujay Phadke <electronicsguy@users.noreply.github.com>
2016-04-25 17:55:44 -0400

    Create HTTPSRedirect.h

---
+++ HTTPSRedirect/HTTPSRedirect.h
@@ -0,0 +1,29 @@
+/*  HTTPS with follow-redirect
+ *  Created by Sujay S. Phadke, 2016
+ *  All rights reserved.
+ *
+ */
+
+#include <WiFiClientSecure.h>
+#include "TextFinder/TextFinder.h"
+
+class HTTPSRedirect : public WiFiClientSecure {
+  private:
+    const int httpsPort;
+    const char* redirFingerprint;
+    bool fpCheck = false;
+    bool keepAlive = true;
+    bool verboseInfo = false;
+    
+  public:
+    HTTPSRedirect(const int, const char*, bool);
+    HTTPSRedirect(const int);
+    ~HTTPSRedirect();
+
+    bool printRedir(const char*, const char*, const char*);
+    bool printRedir(String&, const char*, const char*);
+    String createRequest(const char*, const char*);
+    void fetchData(bool, bool);
+  
+};
+
