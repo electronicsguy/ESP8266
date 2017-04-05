a0864304c7d6531e9b2635accc1b899ae909bd69
Sujay Phadke <electronicsguy123@gmail.com>
2017-04-06 00:12:21 +0530

    fix const char * warnings

--- HTTPSRedirect/HTTPSRedirect.h
+++ HTTPSRedirect/HTTPSRedirect.h
@@ -20,7 +20,7 @@ class HTTPSRedirect : public WiFiClientSecure {
     String _redirUrl;
     String _redirHost;
     unsigned int _maxRedirects;  // to-do
-    char* _contentTypeHeader;
+    const char* _contentTypeHeader;
     
     struct headerFields{
       String transferEncoding;
@@ -78,7 +78,7 @@ class HTTPSRedirect : public WiFiClientSecure {
     void setPrintResponseBody(bool);
     void setMaxRedirects(const unsigned int);
     
-    void setContentTypeHeader(char *);
+    void setContentTypeHeader(const char *);
 #ifdef OPTIMIZE_SPEED
     bool reConnectFinalEndpoint(void);
 #endif
