2cf2d41f654cb7cd1024a0790086c3e42ecb87c9
Sujay Phadke <electronicsguy123@gmail.com>
2018-05-20 21:03:11 +0530

    Update fingerprint.bat

--- HTTPSRedirect/fingerprint.bat
+++ HTTPSRedirect/fingerprint.bat
@@ -6,7 +6,7 @@ REM save cwd
 pushd %cd%
 
 REM put the correct path here for the openssl binary
-cd /D "C:\Users\Sujay Phadke\Downloads\Laptop\openssl-1.0.2k-x64_86-win64\"
+cd /D "<...>"
 
 echo | openssl s_client -connect script.google.com:443 | openssl x509 -fingerprint -noout
 
