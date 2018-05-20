79cd5f8fb2f6f14860eb4c8e6faeb394362a8173
Sujay Phadke <electronicsguy123@gmail.com>
2018-05-20 21:02:44 +0530

    small enhancements

--- HTTPSRedirect/fingerprint.bat
+++ HTTPSRedirect/fingerprint.bat
@@ -1,4 +1,14 @@
+@echo off
+
+REM Sujay Phadke (C) 2018
+
+REM save cwd
+pushd %cd%
+
 REM put the correct path here for the openssl binary
-cd <....>\openssl-0.9.8h-1-bin\bin
+cd /D "C:\Users\Sujay Phadke\Downloads\Laptop\openssl-1.0.2k-x64_86-win64\"
 
 echo | openssl s_client -connect script.google.com:443 | openssl x509 -fingerprint -noout
+
+REM return back to the original working directory
+popd
